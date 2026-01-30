1fec7b6d8a975c6fc99265737aa7f4727ecd7ef9
lorenzotrogublendstudio <lorenzo.blenstudio@gmail.com>
2026-01-30 10:10:38 +0100

    deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // AGGIUNGI QUESTO (deve corrispondere al nome del repository GitHub)
-  base: '/scaravella/', 
+  // Base per il deploy in produzione (root)
+  base: '/',
 })
